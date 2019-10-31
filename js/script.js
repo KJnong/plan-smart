71a4f27ee710b8454773db8fc9bb30a138b750e6
Jethro Nong <nongjethro@gmail.com>
2019-10-31 20:44:37 +0200

    added all files

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function registerUser(){
+    
+    let user ={};
+    user.name = document.getElementById('firstname').value
+    user.lastName = document.getElementById('lastname').value
+    user.email = document.getElementById('email').value
+    user.username = document.getElementById('username').value
+    user.password = document.getElementById('password').value
+
+    console.log(user)
+
+    let http = new XMLHttpRequest();
+    http.open("POST", "https://localhost:44301/api/values")
+    http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    http.send(JSON.stringify(user))
+}
+
+
+var form = document.querySelector('#regForm');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    registerUser()
+} )
+
