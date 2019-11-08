810d1ef59e327b4e91612c0d61494a2b9cf682a0
Jethro Nong <nongjethro@gmail.com>
2019-11-08 14:39:53 +0200

    fixed onload function

--- js/register.js
+++ js/register.js
@@ -10,30 +10,18 @@ function registerUser(){
     console.log(user)
 
     let http = new XMLHttpRequest();
-
-    console.log('Ready state (A)='+ http.readyState);
-    console.log('status (A)' + http.status);
-
     http.open("POST", "https://localhost:44301/api/values" , true)
     http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-    console.log(http);
-    
-    console.log('Ready state (B)='+ http.readyState);
-    console.log('status (B)' + http.status);
-    
     http.onload = () =>{
-        if(status == 200)
+        if( http.status == 200)
             console.log(http.responseText);
 
-        else if(status == 404)
+        else if(http.status == 404)
              console.log(http.responseText);
     }
-    http.send(JSON.stringify(user))
-    console.log('Ready state (C)='+ http.readyState);
-    console.log('status (C)' + http.status);
 
-    
+    http.send(JSON.stringify(user))   
 }
 var form = document.querySelector('#regForm');
 
